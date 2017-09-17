8f3484c58073a4ce1e6bf8a1b723775a27cc5b65
Cher Huang <ib.gaga@hotmail.com>
2017-09-17 23:00:24 +0800

    adjust color for tic tac toe

--- app/containers/ContactPage/index.js
+++ app/containers/ContactPage/index.js
@@ -8,10 +8,7 @@ import Helmet from 'react-helmet';
 import {FormattedMessage } from 'react-intl';
 import H1 from 'components/H1';
 import messages from './messages';
-import List from './List';
-import ListItem from './ListItem';
 require('./ReactForm.scss');
-import ListItemTitle from './ListItemTitle';
 
 class ReactFormLabel extends React.Component {
   constructor() {
